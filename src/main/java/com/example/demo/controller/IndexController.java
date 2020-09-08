44cd10e0791569d585b0ef8d7f83cc34c79ca1f0
keyiliao <xyzm3554@163.com>
2020-09-08 14:53:22 +0800

    更新1

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 	@GetMapping("/hello")
 	public String sayHello() {
+		System.out.println("更新1");
 		return "Hello World!";
 	}
 }
