c111f31042b9a593336386876667a5add96aafc0
keyiliao <30435313+fenxiangmuzuo@users.noreply.github.com>
2020-09-08 15:24:53 +0800

    在线添加二

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -12,6 +12,7 @@ public class IndexController {
 		System.out.println("主干添加");
 		System.out.println("分支添加");
 		System.out.println("在线添加");
+		System.out.println("在线添加2");
 		return "Hello World!";
 	}
 }
